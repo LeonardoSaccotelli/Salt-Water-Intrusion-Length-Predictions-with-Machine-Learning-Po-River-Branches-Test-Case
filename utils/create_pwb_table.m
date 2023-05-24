667e0fdddbc388aff8b8a3c5552b949a22141707
LeonardoSaccotelli <leosaccotelli96@gmail.com>
2023-05-24 11:54:30 +0200

    Updated script 'create_pwb_table.m'

--- utils/create_pwb_table.m
+++ utils/create_pwb_table.m
@@ -1,10 +1,23 @@
-function [pwbTable] = create_pwb_table(obs,pred, pwbTable,algorithm_name, pwbX)
+function [pwbTable] = create_pwb_table(obs, pred, mlAlgName)
     if istable(obs)
         obs = table2array(obs);
     end
     if istable(pred)
         pred = table2array(pred);
     end
+
+    pwbX = [1 5 10 20 30];
+    pwbXRowNames = string();
+    
+    for i = 1:numel(pwbX)
+        pwbXRowNames(i) = strcat('PWB', num2str(pwbX(i)));
+    end
+    
+    pwbTable = table('Size',[numel(pwbX) 1],...
+        'VariableTypes', repmat({'double'}, 1, 1), ...
+        'VariableNames', mlAlgName,...
+        'RowNames', pwbXRowNames);
+
     for i=1:height(pwbTable)
         pwbTable(i,algorithm_name) = {round(computePWBTable(obs, pred, pwbX(i)),2)};
     end
